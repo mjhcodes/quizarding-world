b728e122c822edf5cc0c0d940f3c1756dabb85ed
Matthew Hughes <mjhughes707@gmail.com>
2021-06-15 16:02:21 -0700

    implemented background toggle test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { connect } from "react-redux";
+import * as DK from "./redux/dataKeys";
+import "./App.css";
 
-function App() {
+function App({ dispatch, background }: { dispatch: any; background: string }) {
+  function setBackground(background: string) {
+    dispatch({
+      type: "SET_BACKGROUND",
+      name: "background",
+      value: background,
+    });
+  }
+
+  const backgroundImage = `url(/images/backgrounds/${background}.png)`;
+
   return (
-    <div className="App">
-    </div>
+    <main style={{ backgroundImage }}>
+      <button onClick={() => setBackground(DK.HOGWARTS)}>Hogwarts</button>
+      <button onClick={() => setBackground(DK.GRYFFINDOR)}>Gryffindor</button>
+      <button onClick={() => setBackground(DK.HUFFLEPUFF)}>Hufflepuff</button>
+      <button onClick={() => setBackground(DK.RAVENCLAW)}>Ravenclaw</button>
+      <button onClick={() => setBackground(DK.SLYTHERIN)}>Slytherin</button>
+    </main>
   );
 }
 
-export default App;
+export default connect(
+  (state: any) => ({ background: state.ui.background }),
+  (dispatch) => ({ dispatch })
+)(App);
