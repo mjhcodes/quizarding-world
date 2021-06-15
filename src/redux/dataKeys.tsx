f8909536e82af16ed0b3e9aef76a8ef2a90f0c2d
Matthew Hughes <mjhughes707@gmail.com>
2021-06-15 15:59:57 -0700

    set up starter data keys

---
+++ src/redux/dataKeys.tsx
@@ -0,0 +1,11 @@
+// state - ui
+export const BACKGROUND = "background";
+
+// houses
+export const GRYFFINDOR = "gryffindor";
+export const HUFFLEPUFF = "hufflepuff";
+export const RAVENCLAW = "ravenclaw";
+export const SLYTHERIN = "slytherin";
+
+// hogwarts is my home :-)
+export const HOGWARTS = "hogwarts";
