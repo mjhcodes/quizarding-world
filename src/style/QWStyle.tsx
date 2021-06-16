e8c504829034803e7291fee4aba68bbfbaf112d7
Matthew Hughes <mjhughes707@gmail.com>
2021-06-15 22:56:27 -0700

    set up consistent ui styles

--- src/style/QWStyle.tsx
+++ src/style/QWStyle.tsx
@@ -1,8 +1,10 @@
 import deepFreeze from "deep-freeze";
 import colors from "./colors";
+import ui from "./ui";
 
 const GRStyle = deepFreeze({
   colors,
+  ui,
 });
 
 export default GRStyle;
---
+++ src/style/ui.tsx
@@ -0,0 +1,5 @@
+const ui = {
+  mobileBreakpoint: 769,
+};
+
+export default ui;
