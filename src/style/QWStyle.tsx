438686778e5a6335cd91a4714fbe0efd78cdce2a
Matthew Hughes <mjhughes707@gmail.com>
2021-06-15 18:01:34 -0700

    set up project colors

---
+++ src/style/QWStyle.tsx
@@ -0,0 +1,8 @@
+import deepFreeze from "deep-freeze";
+import colors from "./colors";
+
+const GRStyle = deepFreeze({
+  colors,
+});
+
+export default GRStyle;
---
+++ src/style/colors.tsx
@@ -0,0 +1,18 @@
+export const color = (r: number, g: number, b: number) => {
+  return (a = 1) => {
+    return "rgba(" + [r, g, b, a].join(",") + ")";
+  };
+};
+
+const colors = {
+  White: color(243, 243, 243), // #f3f3f3
+  Grey: color(221, 221, 221), // #dddddd
+  Black: color(0, 0, 0), // #000000
+  Red: color(115, 2, 12), // #73020c
+  Green: color(20, 104, 73), // #146849
+  Blue: color(20, 70, 130), // #144682
+  Yellow: color(229, 196, 111), // #e5c46f
+  Gold: color(217, 181, 4), // #d9b504
+};
+
+export default colors;
