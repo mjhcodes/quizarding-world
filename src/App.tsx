d9cf5dc1b5149f6b4962d11e13fea1395ab34a2c
Matthew Hughes <mjhughes707@gmail.com>
2021-06-15 18:32:12 -0700

    implemented new changes for simple ui testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,50 @@
 import React from "react";
 import { connect } from "react-redux";
 import * as DK from "./redux/dataKeys";
+import { setBackground, toggleLoading } from "./utils/uiUtil";
+import { LoadingIcon, LoadingIconWithWords } from "./components/LoadingIcons";
 import "./App.css";
 
-function App({ dispatch, background }: { dispatch: any; background: string }) {
-  function setBackground(background: string) {
-    dispatch({
-      type: "SET_BACKGROUND",
-      name: "background",
-      value: background,
-    });
-  }
-
+function App({
+  dispatch,
+  background,
+  isLoading,
+}: {
+  dispatch: any;
+  background: string;
+  isLoading: boolean;
+}) {
   const backgroundImage = `url(/images/backgrounds/${background}.png)`;
 
   return (
     <main style={{ backgroundImage }}>
-      <button onClick={() => setBackground(DK.HOGWARTS)}>Hogwarts</button>
-      <button onClick={() => setBackground(DK.GRYFFINDOR)}>Gryffindor</button>
-      <button onClick={() => setBackground(DK.HUFFLEPUFF)}>Hufflepuff</button>
-      <button onClick={() => setBackground(DK.RAVENCLAW)}>Ravenclaw</button>
-      <button onClick={() => setBackground(DK.SLYTHERIN)}>Slytherin</button>
+      <button onClick={() => setBackground(dispatch, DK.HOGWARTS)}>
+        Hogwarts
+      </button>
+      <button onClick={() => setBackground(dispatch, DK.GRYFFINDOR)}>
+        Gryffindor
+      </button>
+      <button onClick={() => setBackground(dispatch, DK.HUFFLEPUFF)}>
+        Hufflepuff
+      </button>
+      <button onClick={() => setBackground(dispatch, DK.RAVENCLAW)}>
+        Ravenclaw
+      </button>
+      <button onClick={() => setBackground(dispatch, DK.SLYTHERIN)}>
+        Slytherin
+      </button>
+      <br />
+      {isLoading ? <LoadingIcon /> : <LoadingIconWithWords />}
+      <button onClick={() => toggleLoading(dispatch, true)}>TRUE</button>
+      <button onClick={() => toggleLoading(dispatch, false)}>FALSE</button>
     </main>
   );
 }
 
 export default connect(
-  (state: any) => ({ background: state.ui.background }),
+  (state: any) => ({
+    background: state.ui.background,
+    isLoading: state.ui.isLoading,
+  }),
   (dispatch) => ({ dispatch })
 )(App);
