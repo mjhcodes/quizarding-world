9d4de866b63081519e36085285e44e6a673a646b
Matthew Hughes <mjhughes707@gmail.com>
2021-06-17 10:37:30 -0700

    set up toggling between menu sections

--- src/overlays/GameSelection.tsx
+++ src/overlays/GameSelection.tsx
@@ -1,25 +1,40 @@
 import React from "react";
-import MenuContainer from "../components/MenuContainer";
+import { connect } from "react-redux";
 import MenuButton from "../components/MenuButton";
+import MenuContainer from "../components/MenuContainer";
+import { toggleOverlay } from "../utils/uiUtil";
 
-export default function GameSelection() {
+function selectFreePlay(dispatch: any) {
+  toggleOverlay(dispatch, "gameSelection", false);
+  toggleOverlay(dispatch, "characterSelection", true);
+}
+
+function GameSelection({ dispatch }: { dispatch: any }) {
   return (
     <MenuContainer>
       <MenuButton
         disabled={false}
         text="FREE PLAY"
         caption="Random Characters - Progress Not Saved"
+        onClick={() => selectFreePlay(dispatch)}
       />
       <MenuButton
         disabled={true}
         text="LOGIN"
-        caption="Select Characters - Collect Items - Compete for House Cup"
+        caption="Coming Soon!"
+        // caption="Select Characters - Collect Items - Compete for House Cup"
       />
       <MenuButton
         disabled={true}
         text="SIGN UP"
-        caption="Create New Account for Free"
+        caption="Coming Soon!"
+        // caption="Create New Account for Free"
       />
     </MenuContainer>
   );
 }
+
+export default connect(
+  (state: any) => ({}),
+  (dispatch) => ({ dispatch })
+)(GameSelection);
