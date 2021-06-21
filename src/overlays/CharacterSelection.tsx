d3624fd8e8b38d7f0a3a6833c89041fb3c87df19
Matthew Hughes <mjhughes707@gmail.com>
2021-06-21 09:46:16 -0700

    updated button/caption and title displays

--- src/overlays/CharacterSelection.tsx
+++ src/overlays/CharacterSelection.tsx
@@ -1,21 +1,42 @@
 import React from "react";
+import { connect } from "react-redux";
 import styled from "styled-components";
 import CharacterButton from "../components/CharacterButton";
+import CharacterCaption from "../components/CharacterCaption";
 import CharacterWindow from "../components/CharacterWindow";
 import MenuContainer from "../components/MenuContainer";
 import QWStyle from "../style/QWStyle";
+import { CharacterSelectionProps } from "../typings/interface";
+import { getRandomExpression } from "../utils/util";
 
 const Title = styled.h2`
   text-align: center;
+  font-size: 1.75rem;
   color: ${QWStyle.colors.Black()};
 `;
 
-export default function CharacterSelection() {
+function CharacterSelection({ isCharacterSelected }: CharacterSelectionProps) {
+  const title = isCharacterSelected
+    ? getRandomExpression()
+    : "POLYJUICE STATION";
+
   return (
     <MenuContainer>
-      <Title>POLYJUICE STATION</Title>
+      <Title>{title}</Title>
       <CharacterWindow />
-      <CharacterButton caption="Click above to drink the potion" />
+
+      {isCharacterSelected ? (
+        <CharacterCaption />
+      ) : (
+        <CharacterButton caption="Click the button above to drink the potion" />
+      )}
     </MenuContainer>
   );
 }
+
+export default connect(
+  (state: any) => ({
+    isCharacterSelected: state.form.isCharacterSelected,
+  }),
+  (dispatch) => ({ dispatch })
+)(CharacterSelection);
