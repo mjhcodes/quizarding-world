0ee165d296d7d7e4ec50ce9b8163fca502870404
Matthew Hughes <mjhughes707@gmail.com>
2021-06-21 17:35:23 -0700

    created basic layout for the main game

--- src/overlays/GameScreen.tsx
+++ src/overlays/GameScreen.tsx
@@ -1,3 +1,38 @@
+import styled from "styled-components";
+import Container from "../components/Game/Container";
+import TotalPoints from "../components/Game/TotalPoints";
+
+const TopSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const MainSection = styled.div`
+  display: flex;
+`;
+
+const BottomSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
 export default function GameScreen() {
-  return <h1>GAME SCREEN</h1>;
+  return (
+    <Container>
+      <TopSection>
+        <TotalPoints />
+        <h1>4 badges</h1>
+      </TopSection>
+      <MainSection>
+        <h1>center</h1>
+      </MainSection>
+      <BottomSection>
+        <h1>triangle</h1>
+        <h1>wands</h1>
+        <h1>home</h1>
+      </BottomSection>
+    </Container>
+  );
 }
