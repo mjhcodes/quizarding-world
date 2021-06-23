2f2cd931ff2c721dddd36f3f2f37922a54a8464e
Matthew Hughes <mjhughes707@gmail.com>
2021-06-23 11:40:00 -0700

    styling updates

--- src/components/Game/Question.tsx
+++ src/components/Game/Question.tsx
@@ -5,26 +5,35 @@ import styled from "styled-components";
 import QWStyle from "../../style/QWStyle";
 import { QuestionProps } from "../../typings/interface";
 
+interface StyleProps {
+  isMobile?: boolean;
+}
+
 const QuestionWrapper = styled.div`
   display: flex;
+  align-items: center;
   justify-content: center;
   padding: 1.5rem 2.5rem;
   width: 100%;
+  min-height: 100px;
   text-align: center;
-  line-height: 1.25rem;
+  font-size: ${({ isMobile }: StyleProps) => (isMobile ? "1rem" : "1.25rem")};
+  line-height: ${({ isMobile }: StyleProps) =>
+    isMobile ? "1.25rem" : "1.5rem"};
   border-radius: 50px;
   border: 5px solid ${QWStyle.colors.Black()};
   background-color: ${QWStyle.colors.Grey()};
   box-shadow: 20px 20px 20px 0px ${QWStyle.colors.Black(0.25)};
 `;
 
-function Question({ current_question }: QuestionProps) {
+function Question({ isMobile, current_question }: QuestionProps) {
   const question = _.get(current_question, "question", "");
-  return <QuestionWrapper>{question}</QuestionWrapper>;
+  return <QuestionWrapper isMobile={isMobile}>{question}</QuestionWrapper>;
 }
 
 export default connect(
   (state: any) => ({
+    isMobile: state.ui.isMobile,
     current_question: state.form.game.current_question,
   }),
   (dispatch) => ({ dispatch })
