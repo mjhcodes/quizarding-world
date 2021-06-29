ec68400f9546c1179abdd9e231e3720f87aa56c8
Matthew Hughes <mjhughes707@gmail.com>
2021-06-29 08:44:20 -0700

    randomly selects and stores 21 questions at app load

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,46 @@
 import _ from "lodash";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { connect } from "react-redux";
 import "./App.css";
 import RenderOverlays from "./overlays";
 import { fetchData } from "./services/fetchData";
 import QWStyle from "./style/QWStyle";
 import { AppProps } from "./typings/interface";
+import { setRemainingQuestions } from "./redux/formActions";
 import { toggleValue } from "./utils/uiUtil";
+import db from "./db.json";
+
+function getRemainingQuestions(
+  dispatch: any,
+  questions: object[],
+  total_rounds: number
+) {
+  const remaining_questions: object[] = [];
+  const used_question_ids: number[] = [];
+
+  while (remaining_questions.length < total_rounds) {
+    const random_id = Math.floor(Math.random() * questions.length);
+
+    if (!_.includes(used_question_ids, random_id)) {
+      used_question_ids.push(random_id);
+      remaining_questions.push(questions[random_id]);
+    }
+  }
+
+  setRemainingQuestions(dispatch, remaining_questions);
+}
 
 function App({
   dispatch,
-  background,
-  game_screen,
   is_character_selected,
   selected_character,
+  remaining_questions,
+  total_rounds,
+  background,
+  game_screen,
 }: AppProps) {
+  const questions = db.questions;
+
   window.onresize = () => {
     window.innerWidth < QWStyle.ui.mobileBreakpoint
       ? toggleValue(dispatch, "isMobile", true)
@@ -29,6 +55,10 @@ function App({
 
   useEffect(() => {
     fetchData(dispatch);
+
+    if (_.isEmpty(remaining_questions)) {
+      getRemainingQuestions(dispatch, questions, total_rounds);
+    }
   }, [dispatch]);
 
   const house = _.get(selected_character, "house", background);
@@ -44,10 +74,12 @@ function App({
 
 export default connect(
   (state: any) => ({
-    background: state.ui.background,
-    game_screen: state.ui.overlayVisibility.game_screen,
     is_character_selected: state.form.character.is_character_selected,
     selected_character: state.form.character.selected_character,
+    remaining_questions: state.form.game.remaining_questions,
+    total_rounds: state.form.game.total_rounds,
+    background: state.ui.background,
+    game_screen: state.ui.overlayVisibility.game_screen,
   }),
   (dispatch) => ({ dispatch })
 )(App);
